b1ddec982ee7847e4f8b42da739dceb50a53d448
Michael Wright <mkwright@gmail.com>
2015-01-27 17:09:17 -0700

    Added the json object code to it

--- js-objects/todo.js
+++ js-objects/todo.js
@@ -1,21 +1,32 @@
+// Sample code from class todo
 'use strict';
 
 var todoItems = [];
 var todoName;
+var todoDesc;
 var todoList;
 
 function pageInit() {
   document.getElementById('todoForm').onsubmit = addNewTodo;
   todoList = document.getElementById('todoList');
   todoName = document.getElementById('todoName');
+  todoDesc = document.getElementById('todoDesc');
 }
 
 function addNewTodo() {
   console.log('Adding a new item');
-  todoItems.push(todoName.value);
+  var todo = {
+    name: todoName.value,
+    desc: todoDesc.value
+  };
+  todoItems.push(todo);
+
   todoList.innerHTML = "";
   for (var i = 0; i < todoItems.length; ++i) {
-    todoList.innerHTML += "<div><input type='checkbox'>" + todoItems[i] + "</input></div>";
+    todoList.innerHTML += "<div>" +
+    "<input type='checkbox'>" + todoItems[i].name + "</input>" +
+    "<span>" + todoItems[i].desc + "</span>"
+    "</div>";
   }
   todoName.value = "";
 
